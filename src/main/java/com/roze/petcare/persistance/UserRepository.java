426abcd3c4ed9eb120869478500acd846353893f
ruta.rozene <rozene.ruta@gmail.com>
2024-06-22 23:42:00 +0300

    security-setup: added additional method to find user by email

--- src/main/java/com/roze/petcare/persistance/UserRepository.java
+++ src/main/java/com/roze/petcare/persistance/UserRepository.java
@@ -4,8 +4,11 @@ import com.roze.petcare.persistance.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     boolean existsByEmailIgnoreCase(String email);
+    Optional<UserEntity> findByEmail(String email);
 }
